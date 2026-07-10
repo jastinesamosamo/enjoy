caec1efa4e45b4c88077819462c9961db41da0c0
jastinesamosamo <jastinesamo@gmail.com>
2026-07-10 18:57:16 +0300

    Add PWA manifest and service worker

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "enjoy-v1";
+
+const FILES_TO_CACHE = [
+  "/enjoy/",
+  "/enjoy/index.html",
+  "/enjoy/dotto.css",
+  "/enjoy/dotto.js",
+  "/enjoy/admin.html",
+  "/enjoy/admin.css",
+  "/enjoy/admin.js",
+  "/enjoy/myrequests.html",
+  "/enjoy/myrequests.js",
+  "/enjoy/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
